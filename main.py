f5d83d3b7d5f69bf0ee9a8dc3d8c80008a3327ea
Kevin <kzone159@gmail.com>
2026-01-11 00:40:47 -0600

    adding message body

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import json
 with open("password.json","r") as file:
+    data = json.load(file)
 
 
-SMTP_SERVER = 
+message = MIMEMultipart()
+message["From"] = data["sender_email"]
+message["To"] = data["reciever_email"]
+# SMTP_SERVER = 
