cb4d7346874dc20cf8fbe8935d42392be6120ac1
Kevin <kzone159@gmail.com>
2026-01-11 01:05:56 -0600

    code is sound except it won't send

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import json
 with open("password.json","r") as file:
     data = json.load(file)
 
+SMTP_SERVER = data["server"]
+PASSWORD = data["password"]
+PORT = data["port"]
+USERNAME = data["sender_email"]
+Reciever_email = data["reciever_email"]
+body = "Holy cow can this actually go through?"
+message = MIMEText(body,"plain")
 
-message = MIMEMultipart()
 message["From"] = data["sender_email"]
 message["To"] = data["reciever_email"]
-# SMTP_SERVER = 
+message["Subject"] = "Sending this via python"
+
+
+
+
+with smtplib.SMTP(SMTP_SERVER,PORT) as server:
+    server.starttls()
+    server.login(USERNAME,PASSWORD)
+    server.sendmail(USERNAME,Reciever_email,message.as_string())
